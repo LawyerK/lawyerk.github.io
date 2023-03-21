fda8850636ab09d1354545b6cedb509202a171d7
mrsonic <mr.sonicmaster13@gmail.com>
2023-03-21 04:22:07 -0400

    updates

---
+++ assets/js/snek.js
@@ -0,0 +1,135 @@
+const canvas = document.getElementById('game'),
+    context = canvas.getContext('2d');
+
+let grid = 16,
+    score = 0,
+    max = 0,
+    mul = 0,
+    ivl;
+
+const snake = {
+    x: 160,
+    y: 160,
+    dx: grid,
+    dy: 0,
+    cells: [],
+    maxCells: 4
+};
+
+const food = {
+    x: 320,
+    y: 320
+};
+
+const comp_cksum = (x) => (Math.imul(x ^ 0xDEADBEEF, 0xCAFEBABE) ^ 0xDEADC0DE) >>> 0;
+const rand = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+const wrap = (n, m) => (n % m + m) % m;
+
+function set_score(new_score) {
+    score = new_score;
+
+    document.getElementById('score').innerHTML = `&nbsp;${score}`;
+
+    let new_mul = Math.floor(score / 5);
+
+    if (new_mul != mul) {
+        clearInterval(ivl);
+        mul = new_mul;
+        const speed = Math.min(60, 10 + 2 * mul);
+        ivl = setInterval(loop, 1000 / speed);
+        console.log(speed);
+    }
+}
+
+function loop() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    snake.x = wrap(snake.x + snake.dx, canvas.width);
+    snake.y = wrap(snake.y + snake.dy, canvas.height);
+
+    snake.cells.unshift({ x: snake.x, y: snake.y });
+
+    if (snake.cells.length > snake.maxCells)
+        snake.cells.pop();
+
+    context.shadowBlur = 20;
+    context.shadowColor = '#eeeeee33';
+
+    context.fillStyle = 'white';
+    context.fillRect(food.x, food.y, grid - 1, grid - 1);
+
+    context.fillStyle = '#E43F5A';
+
+    snake.cells.forEach((cell, index) => {
+        context.fillRect(cell.x, cell.y, grid - 1, grid - 1);
+
+        if (cell.x === food.x && cell.y === food.y) {
+            food.x = rand(0, 25) * grid;
+            food.y = rand(0, 25) * grid;
+
+            set_score(score + 1);
+            snake.maxCells++;
+        }
+
+        for (var i = index + 1; i < snake.cells.length; i++) {
+            if (cell.x === snake.cells[i].x && cell.y === snake.cells[i].y) {
+                max = Math.max(score, max);
+                document.getElementById('high').innerHTML = '&nbsp;' + max;
+                set_score(0);
+
+                food.x = rand(0, 25) * grid;
+                food.y = rand(0, 25) * grid;
+                snake.x = 160;
+                snake.y = 160;
+                snake.cells.length = 0;
+                snake.maxCells = 4;
+                snake.dx = grid;
+                snake.dy = 0;
+            }
+        }
+    });
+}
+
+let leet = ['1', '3', '3', '7'], teel = 0;
+
+document.addEventListener('keydown', e => {
+    let k = e.key.toLowerCase();
+    if (k === 'w' && snake.dy === 0) {
+        snake.dy = -grid;
+        snake.dx = 0;
+    } else if (k === 'a' && snake.dx === 0) {
+        snake.dx = -grid;
+        snake.dy = 0;
+    } else if (k === 's' && snake.dy === 0) {
+        snake.dy = grid;
+        snake.dx = 0;
+    } else if (k === 'd' && snake.dx === 0) {
+        snake.dx = grid;
+        snake.dy = 0;
+    }
+
+    if (k == leet[teel]) {
+        if (++teel == leet.length) {
+            alert('HACKER!');
+            set_score(Math.round((score + 1337) / 1337) * 1337);
+            teel = 0;
+        }
+    }
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    max = localStorage.getItem('max_score') || 0;
+    let cksum = localStorage.getItem('cksum');
+
+    if (comp_cksum(max) != cksum) {
+        localStorage.removeItem('max_score');
+        localStorage.removeItem('cksum');
+        cksum = 0;
+        max = 0;
+    }
+
+    document.getElementById('high').innerHTML = '&nbsp;' + max;
+});
+
+ivl = setInterval(loop, 1000 / 10);
+console.log('set ivl', ivl);
--- assets/js/snow.js
+++ assets/js/snow.js
@@ -1,7 +1,21 @@
-const content = document.querySelector('#snow-container');
+const container = document.querySelector('#snow-container');
 
 for (let i = 0; i < 200; i++) {
     const snow = document.createElement('div');
     snow.className = 'snow';
-    content.append(snow);
+    container.append(snow);
 }
+
+/* there's probably a pure-css fix but this is so much easier lol
+ * edit: except oof it go zoom zoom */
+document.addEventListener('DOMContentLoaded', () => {
+    const body = document.body,
+        html = document.documentElement;
+
+    const height = Math.max(body.scrollHeight, body.offsetHeight,
+        html.clientHeight, html.scrollHeight, html.offsetHeight);
+    const height_max = height / innerHeight * 100;
+
+    container.style.setProperty('--height-max', `${height_max}vh`);
+    console.log(height_max);
+});
